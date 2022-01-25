230ed5b6020f82b3118706ee476d020c0f5ec8e0
tanishq-mehta <74287480+tanishq-mehta@users.noreply.github.com>
2022-01-25 19:12:42 +0530

    Add files via upload

---
+++ gdsc project/page.js
@@ -0,0 +1,30 @@
+/*if(window.location == '#page2'){
+    document.getElementById("link2").style.textDecoration ="underline";
+}*/
+
+function handleOver()   
+{  
+     document.getElementById('button1').style.backgroundColor = 'rgb(214,162,146)';  
+}  
+
+function handleOut()   
+{  
+     document.getElementById('button1').style.backgroundColor = 'rgb(183,130,118)';  
+}  
+
+function handleOver2()   
+{  
+     document.getElementById('button2').style.backgroundColor = 'rgb(214,162,146)';  
+}  
+
+function handleOut2()   
+{  
+     document.getElementById('button2').style.backgroundColor = 'rgb(183,130,118)';  
+}  
+
+
+if(window.location=="#page2"){
+   // document.getElementById('link2').style.textDecoration = 'underline';
+    alert("page2");
+}
+
